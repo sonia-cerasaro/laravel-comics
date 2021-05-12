e3d1feb7b261352c1736b86a92c3f0359292bc90
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-05-12 16:26:38 +0200

    sass

---
+++ resources/views/info.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('main')
+<p>INFOO</p>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+
+  <ul>
+    <li>
+      <a href="/info">
+        INFO
+      </a>
+    </li>
+  </ul>
+
+</header>
